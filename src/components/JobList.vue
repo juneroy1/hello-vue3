38d6a60ea5f27397a66a68202475e0fe89c53a5d
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-30 00:22:05 +0800

    feat added job salary and description display

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="job-list">
     <ul>
-        <li v-for="job in jobs" :key="job.id">
-            <h2>{{ job.title }} in {{ job.location }}</h2>
-            
-        </li>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} PHP</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus vel et odio illo incidunt, amet voluptas dicta
+            temporibus animi neque sit error deleniti rem dolor ipsa quas harum
+            dolorem voluptates?
+          </p>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -14,12 +24,12 @@ import Job from "@/types/Job";
 import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
-    props: {
-        jobs: {
-            required: true,
-            type: Array as PropType<Job[]>,
-        }
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
     },
+  },
   setup() {
     return {};
   },

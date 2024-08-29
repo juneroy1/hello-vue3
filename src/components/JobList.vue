382fe4871f9a0314917be814544cbb0d7460b324
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-30 00:21:02 +0800

    feat added job title and location

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="job-list">
     <ul>
-        <li v-for="job in jobs" :key="job.id"></li>
+        <li v-for="job in jobs" :key="job.id">
+            <h2>{{ job.title }} in {{ job.location }}</h2>
+            
+        </li>
     </ul>
   </div>
 </template>

2e5b293f16186f0993514c23d58f631af600966d
Juneroy Quinimon <eroyjune@gmail.com>
2024-08-29 23:34:55 +0800

    feat remove default style

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,6 @@ export default defineComponent({
   name: 'App',
   components: {
   }
-});
+}); 
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
